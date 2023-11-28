c1641b8b0815e21bfe5246ea16faeacc4ab8e919
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-11-28 20:35:24 +0430

    add changes

--- components/About/main_slide.tsx
+++ components/About/main_slide.tsx
@@ -1,25 +1,19 @@
-'use client';
-import pic1 from '../../public/image/pic1.jpg';
-import pic2 from '../../public/image/pic2.jpg';
-import pic3 from '../../public/image/pic3.jpg';
-import pic4 from '../../public/image/pic4.jpg';
-import AboutCarousel from '../About/AboutCrousel';
+// 'use client';
+import pic4 from '../../public/image/about.png';
 import Image from 'next/image';
 
 const AboutSlider = () => {
-  const images = [pic1, pic2, pic3, pic4];
   return (
-    <div>
-      <AboutCarousel loop>
-        {images.map((src, i) => {
-          return (
-            <div className="relative flex-[0_0_100%]" key={i}>
-              {/* use object-cover + fill since we don't know the height and width of the parent */}
-              <Image src={src} className="w-full h-full" alt="alt" />
-            </div>
-          );
-        })}
-      </AboutCarousel>
+    <div className="relative w-full h-160">
+      <div className="absolute bottom-[-5%] left-1/2 transform -translate-x-1/2 bg-white p-10 shadow-xl dark:shadow-6xl ring-white ring-offset-80">
+        <p className="mb-11 text-[#c86500] text-6xl text-center font-bold">
+          25+
+        </p>{' '}
+        <div className="text-[#c86500] mb-10 text-center font-bold text-2xl">
+          Years of Experience{' '}
+        </div>
+      </div>
+      <Image src={pic4} className="w-full h-full" alt="alt" />
     </div>
   );
 };
