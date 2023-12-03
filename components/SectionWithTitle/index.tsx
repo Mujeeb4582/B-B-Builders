3d91b62b11849e036d4e648de6c527926d6e8469
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-12-04 02:29:54 +0500

    fix the animation

--- components/SectionWithTitle/index.tsx
+++ components/SectionWithTitle/index.tsx
@@ -12,15 +12,15 @@ const SectionWithTitle = ({
   <div className="pb-5">
     <div className="bg-headingText text-white text-center py-2 rounded-lg shadow-lg relative">
       {showLabel && (
-        <div className="text-headingText py-2 px-4 text-2xl font-bold bg-yellow-400 absolute rounded-lg top-2 left-56 sales-animations">
+        <p className="text-headingText py-2 px-4 lg:text-2xl font-bold bg-yellow-400 absolute rounded-lg top-2 hidden md:block md:left-56 sales-animations">
           {news}
-        </div>
+        </p>
       )}
       <h2 className="text-3xl font-bold sm:text-4xl lg:text-5xl">{title}</h2>
       {showLabel && (
-        <div className="text-headingText py-2 px-4 text-2xl font-bold bg-yellow-400 absolute rounded-lg top-2 right-48 sales-animations">
+        <p className="text-headingText py-2 px-4 lg:text-2xl font-bold bg-yellow-400 absolute rounded-lg top-2 md:right-48 sales-animations">
           {news}
-        </div>
+        </p>
       )}
     </div>
     {children}
